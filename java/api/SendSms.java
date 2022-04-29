c0d40d3e3994d2e878df80312e851fb9ec35a056
Lee SangHyun <98644961+Tagolea@users.noreply.github.com>
2022-04-29 14:04:58 +0900

    Update SendSms.java

--- java/api/SendSms.java
+++ java/api/SendSms.java
@@ -12,14 +12,14 @@ public class SendSms {
 //사용 한도 14번 ^^
 
 	static public void sendSms(String to, String msg) {
-		String api_key = "NCS8LY0QO0MKM1LP";
-		String api_secret = "JVKE47OD9M3KRUCACPXARHLWOM1STWTL";
+		String api_key = "";
+		String api_secret = "";
 		Message coolsms = new Message(api_key, api_secret);
 
 		// 4 params(to, from, type, text) are mandatory. must be filled
 		HashMap<String, String> params = new HashMap<String, String>();
 		params.put("to", to); // 수신자
-		params.put("from", "01095803041"); // 발신자
+		params.put("from", ""); // 발신자
 		params.put("type", "SMS");
 		params.put("text", "필기 아이디 : " + msg); // 문자내용
 		params.put("app_version", "test app 1.2"); // application name and version
